1570f49b5de079b0e0b451f5ad7f18db5a4fcc9c
Robert Gleason <l2ob122@gmail.com>
2024-04-15 20:54:13 -0700

    setup

---
+++ src/test/java/ca/robertgleason/bankingapp/BankingappApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.robertgleason.bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
